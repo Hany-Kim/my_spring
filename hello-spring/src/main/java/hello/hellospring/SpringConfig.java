b87be33954f40078e657ede20b863759f56adf54
Hany-Kim <hanykim1210@gmail.com>
2025-06-04 01:40:41 +0900

    [중요] JDBC 연결 - Repository 구현체 변경하기 (개방-폐쇄 원칙, 의존성 주입)

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,29 @@
 package hello.hellospring;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    // 방법 1
+//    @Autowired DataSource dataSource;
+
+    // 방법 2
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +32,14 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        /*
+        객체지향 특성 중 '다형성'을 이용한 방식 -> 개방-폐쇄 원칙 : 확장에는 열려있고, 수정(변경)에는 닫혀있다.
+
+        spring DI(의존성 주입) : 기존 코드를 전혀 손대지 않고, 설정만으로 구현클래스를 변경할 수 있다.
+
+        repository 구현체를 바꿔끼우기 할 수 있다.
+         */
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ hello-spring/src/main/java/hello/hellospring/repository/JdbcMemberRepository.java
@@ -0,0 +1,183 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class JdbcMemberRepository implements MemberRepository{
+
+    private final DataSource dataSource; // DB 연결 위한
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "INSERT INTO member(name) values(?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS); // RETURN_GENERATED_KEYS: insert 후 얻는 id값
+
+            pstmt.setString(1, member.getName()); // sql변수 값의 첫번째 ?와 매칭
+
+            pstmt.executeUpdate(); // DB에 update쿼리가 날아가는 시점
+            rs = pstmt.getGeneratedKeys(); // DB에 생성된 키값을 꺼내옴
+
+            if (rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs); // conn, pstmt, rs 자원(리소스)들 반환해야함.
+        }
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        String sql = "select * from member where id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setLong(1, id);
+
+            rs = pstmt.executeQuery(); // DB에 search 쿼리가 날아가는 시점
+
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            } else {
+                return Optional.empty();
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        String sql = "select * from member where name = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, name);
+
+            rs = pstmt.executeQuery();
+
+            if(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                return Optional.of(member);
+            }
+
+            return Optional.empty();
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    @Override
+    public List<Member> findAll() {
+        String sql = "select * from member";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            rs = pstmt.executeQuery();
+
+            List<Member> members = new ArrayList<>();
+            while(rs.next()) {
+                Member member = new Member();
+                member.setId(rs.getLong("id"));
+                member.setName(rs.getString("name"));
+                members.add(member);
+            }
+
+            return members;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
+    private Connection getConnection() {
+        /*
+        JdbcMemberRepository 생성하고 dataSource를 주입받는 시점에
+        dataSource.getConnection();
+        로 커넥션을 유지할 수도 있다. 단, 계속 새로운 커넥션이 주어진다.
+
+        DataSourceUtils.getConnection(dataSource);
+        방식은 스프링에서 제공하는 트랜잭션을 유지해주는 커넥션 획득방법이다.
+         */
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null) {
+                close(conn);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void close(Connection conn) throws SQLException {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+
+}
