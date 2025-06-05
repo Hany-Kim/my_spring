34a6c4eacd767b54f1bade9f914bea59c0ca188f
Hany-Kim <hanykim1210@gmail.com>
2025-06-06 01:25:21 +0900

    순수한 JAVA 사용, 주문과 할인 도메인 개발

---
+++ core/src/main/java/hello/core/discount/DiscountPolicy.java
@@ -0,0 +1,11 @@
+package hello.core.discount;
+
+import hello.core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     * @return 할인 대상 금액 : 얼마가 할인 되었는지
+     */
+    int discount(Member member, int price);
+}
