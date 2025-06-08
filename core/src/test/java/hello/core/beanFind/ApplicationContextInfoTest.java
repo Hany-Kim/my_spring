cf4105f5e57cfb4f3ae55f549d9c0a202076d413
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2025-06-08 17:18:29 +0900

    Spring 컨테이너에 등록한 빈 목록을 확인하는 방법

---
+++ core/src/test/java/hello/core/beanFind/ApplicationContextInfoTest.java
@@ -0,0 +1,38 @@
+package hello.core.beanFind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName); // BeanDefinition : 빈의 메타데이터 정보
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                // BeanDefinition.ROLE_INFRASTRUCTURE : Spring이 자동으로 등록한 빈
+                // BeanDefinition.ROLE_APPLICATION : Spring이 자동으로 등록한 빈이 아닌, 개발자가 개발을 위해 등록한 빈
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " object = " + bean);
+            }
+        }
+    }
+}
