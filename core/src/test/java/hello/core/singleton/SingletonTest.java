02430572750c373a152324aaf7fccbd08104d95a
Hany-Kim <hanykim1210@gmail.com>
2025-06-09 18:36:02 +0900

    싱글톤, Spring 없는 순수한 DI 컨테이너

---
+++ core/src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.singleton;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        //1. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+
+        /*
+        문제 : 호출될 때마다 JVM에 서로 다른 객체가 계속 쌓인다.
+        트래픽이 초당 100이면 초당 100개 객체가 생성되고 소멸한다. -> 비효율(메모리 낭비)
+
+        해결 : 해당 객체 딱 1개만 생성하고, 공유되도록 함 -> 싱글톤패턴
+         */
+    }
+}
