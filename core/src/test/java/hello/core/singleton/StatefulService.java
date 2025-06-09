d08986ea30b944809f66d80d832d24987eadf10b
Hany-Kim <hanykim1210@gmail.com>
2025-06-09 20:17:55 +0900

    [중요] 싱글톤, 싱글톤 방식의 문제점 - 해결방법

--- core/src/test/java/hello/core/singleton/StatefulService.java
+++ core/src/test/java/hello/core/singleton/StatefulService.java
@@ -2,14 +2,20 @@ package hello.core.singleton;
 
 public class StatefulService {
 
+    /*
+    해결방법 1.
+    필드 대신 공유되지 않는 지역변수, 파라미터, ThreadLocal을 사용
+
     private int price; // 상태를 유지하는 필드
+     */
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 여기가 문제!
-    }
-
-    public int getPrice() {
+//        this.price = price; // 여기가 문제!
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
--- core/src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ core/src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -2,7 +2,6 @@ package hello.core.singleton;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import hello.core.beanfind.ApplicationContextExtendsFindTest.TestConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -18,24 +17,15 @@ class StatefulServiceTest {
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
         // ThreadA: userA 10000원 주문
-        statefulService1.order("userA", 10000);
+        int userAPrice = statefulService1.order("userA", 10000);
         // ThreadB: userB 20000원 주문
-        statefulService2.order("userB", 20000);
+        int userBPrice = statefulService2.order("userB", 20000);
 
         // ThreadA: userA 주문 금액 조회
-        int price = statefulService1.getPrice();
-        System.out.println("price = " + price);
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAPrice);
 
-        /*
-        문제
-        userA는 10000원 주문했지만, 20000원이 조회된다.
-
-        Thread를 예시에 적용하진 않았지만, 실제 서비스(웹)에선 각 Thread에서 userA, userB의 요청을 처리한다.
-
-        스프링 빈은 무상태(stateless)로 설계해야한다.
-         */
-
-        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+        Assertions.assertThat(userAPrice).isEqualTo(10000);
     }
 
     static class TestConfig {
