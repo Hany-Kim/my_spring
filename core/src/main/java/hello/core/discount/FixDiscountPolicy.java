91db20858b439a476c9d447da65968b9f72357d5
Hany-Kim <hanykim1210@gmail.com>
2025-06-22 00:39:43 +0900

    [중요] 조회 빈이 2개 이상일 때 생기는 문제

--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,16 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+/*
+@Component 로 FixDiscountPolicy 클래스을 Spring Bean으로 등록하면,
+기존에 등록되어 있던 RateDiscountPolicy로 인해
+DiscountPolicy의 구현된 클래스가 FixDiscountPolicy와 RateDiscountPolicy 두 클래스이기에
+OrderServiceImpl의 생성자 주입시, DiscountPolicy의 구현 클래스를 찾을 때 2개의 빈등록으로 인해 오류(NoUniqueBeanDefinitionException)를 반환한다.
+ */
+
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
