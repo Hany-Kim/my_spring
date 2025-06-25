fec3039cd0735a8b9011c5651669a0604e096804
Hany-Kim <hanykim1210@gmail.com>
2025-06-26 05:11:57 +0900

    [중요]Prototype스코프 싱글톤 빈과 함께 사용시 문제점 - 싱글톤에서 Prototype 빈 사용

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,10 +2,12 @@ package hello.core.scope;
 
 import static org.assertj.core.api.Assertions.*;
 
+import hello.core.scope.PrototypeTest.PrototypeBean;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -21,6 +23,46 @@ public class SingletonWithPrototypeTest1 {
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        /*
+        ClientBean은 싱글톤 스코프에 생명주기를 가짐.
+        PrototypeBean은 프로토타입 스코프에 생명주기를 가짐.
+
+        ClientBean은 스프링이 실행될 때 컨테이너에 빈이 등록됨.
+        ClientBean이 생성될 때, PrototypeBean이 의존관계에 의해 함께 생성됨.
+        PrototypeBean은 프로토타입 스코프이기에 생성된 후 ClientBean(클라이언트)에게 관리를 넘김.
+        즉, PrototypeBean은 스프링이 생명주기를 관리하지 않음.
+
+        clientBean1, clientBean2는 같은 ClientBean을 참조함.
+        그렇기 때문에 clientBean1, clientBean2는 같은 PrototypeBean를 참조함. => 문제발생
+         */
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; // 생성시점에 주입
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
     }
 
     @Scope("prototype")
