799aba9f8b7df61beab5a8f3b31d5220fcffac08
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2025-07-05 11:58:03 +0900

    Member 저장

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,11 +6,28 @@ public class JpaMain {
 
     public static void main(String[] args) {
 
+        // 애플리케이션 로딩 시점에 딱 한개만 만들어 둔다.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        // 트랜잭션 단위로 EntityManager를 생성하고 소멸시킨다.
+        EntityManager em = emf.createEntityManager(); // DB 커넥션 하나를 받은것과 유사
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member); // member 저장
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
         emf.close();
     }
 }
