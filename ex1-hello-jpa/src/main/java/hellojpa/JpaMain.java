7688372884f91632f48992d2b14608fda4a63bc1
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2025-07-05 12:40:17 +0900

    [중요] 영속성컨텍스트 2, 1차 캐시에서 조회1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -21,28 +21,34 @@ public class JpaMain {
         try {
             // 비영속 상태 (new / transient)
             Member member = new Member();
-            member.setId(100L);
+            member.setId(101L);
             member.setName("HelloJPA");
 
             // 영속 상태 (managed)
             System.out.println("BEFORE"); // DB에 저장된다면 SQL이 날아감. SQL이 출력되는 시점을 확인
             em.persist(member); // 엄밀히 말해서 DB에 저장하는 것은 아님
             System.out.println("AFTER");
-//                BEFORE
-//                AFTER
-//                Hibernate:
-//                   /* insert for
-//                        hellojpa.Member */insert
-//                                    into
-//                            Member (name, id)
-//                            values
-//                                    (?, ?)
 
-            // 준영속 상태 (detached)
-//            em.detach(member); // 영속성 컨텍스트에서 분리
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName());
 
-            // 삭제 상태 (removed)
-//            em.remove(member); // DB 삭제 요청
+//            BEFORE
+//            AFTER
+//            findMember.id = 101
+//            findMember.name = HelloJPA
+//            Hibernate:
+//                /* insert for
+//                    hellojpa.Member */insert
+//                into
+//                    Member (name, id)
+//                values
+//                    (?, ?)
+            /*
+            위 출력결과를 보면 em.find()가 실행한 뒤 출력 후
+            insert 쿼리가 보내졌다.
+            select 쿼리는 보이지 않는다. -> em.persist()실행 시점에 '1차 캐싱'되어 영속성 컨텍스트에서 데이터를 조회했기 때문
+             */
 
             tx.commit(); // SQL 쿼리가 DB에 날아가는 시점
         } catch (Exception e) {
