45f236ce9382423c8af00cc4eea5562bae0df57a
Hany-Kim <74503250+Hany-Kim@users.noreply.github.com>
2025-07-05 12:31:49 +0900

    [중요] 영속성컨텍스트 1

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import jakarta.persistence.*;
 import java.util.List;
+import jdk.swing.interop.LightweightFrameWrapper;
 
 public class JpaMain {
 
@@ -18,15 +19,32 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            // Member는 테이블에 접근하는 것이 아닌 '객체'에 접근하는 것.
+            // 비영속 상태 (new / transient)
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for (Member member : result) {
-                System.out.println("member.getName() = " + member.getName());
-            }
+            // 영속 상태 (managed)
+            System.out.println("BEFORE"); // DB에 저장된다면 SQL이 날아감. SQL이 출력되는 시점을 확인
+            em.persist(member); // 엄밀히 말해서 DB에 저장하는 것은 아님
+            System.out.println("AFTER");
+//                BEFORE
+//                AFTER
+//                Hibernate:
+//                   /* insert for
+//                        hellojpa.Member */insert
+//                                    into
+//                            Member (name, id)
+//                            values
+//                                    (?, ?)
 
-            tx.commit();
+            // 준영속 상태 (detached)
+//            em.detach(member); // 영속성 컨텍스트에서 분리
+
+            // 삭제 상태 (removed)
+//            em.remove(member); // DB 삭제 요청
+
+            tx.commit(); // SQL 쿼리가 DB에 날아가는 시점
         } catch (Exception e) {
             tx.rollback();
         } finally {
