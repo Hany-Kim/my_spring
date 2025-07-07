20be82da5d26f164c1fc70101145e35ef2c40546
Hany-Kim <hanykim1210@gmail.com>
2025-07-07 15:21:35 +0900

    필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -19,32 +19,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /*
-            * 준영속 상태로 만드는 방법
-            * - em.detach(entity) : 특정 엔티티만 준영속 상태로 전환
-            * - em.clear() : 영속성 컨텍스트를 완전히 초기화
-            * - em.close() : 영속성 컨텍스트를 종료
-            * */
-
-            // 영속
-            Member member = em.find(Member.class, 150L); // -> 가져오면 영속상태가 된다.
-            member.setName("AAAAA"); // 값이 변경되면 '더티 체킹'이 된다.
-
-            em.detach(member); // 준영속상태로 전환 -> DB를 확인하면 데이터가 변경되지 않게 된다.
-//            Hibernate:
-//                select
-//                    m1_0.id,
-//                    m1_0.name
-//                from
-//                    Member m1_0
-//                where
-//                    m1_0.id=?
-//            ===================
-            /*
-            * select 쿼리만 전송되고
-            * update 쿼리는 생기지 않았음을 확인할 수 있다.
-            * */
-
             System.out.println("==================");
             tx.commit(); // SQL 쿼리가 DB에 날아가는 시점
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,38 +1,53 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.persistence.Transient;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+    /*
+    * unique 제약 조건을 @Column에서는 잘 사용하지 않는다.
+    * 제약조건 이름이 랜덤으로 생기기 때문에 예외가 발생할 때,
+    * 파악하기 번거롭다.
+    *
+    * unique 제약조건은 @Table의 uniqueConstraints를 사용해
+    * 제약조건 이름을 직접 명명하여 사용한다.
+    * */
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description; // VARCHAR보다 더 긴 문자열 사용할 때
+    
+    @Transient
+    private int temp; // DB와 매핑하지 않고 메모리에서만 사용할 떄
 
     public Member() {
 //    JPA는 내부적으로 리플렉션을 쓰기 때문에 동적으로 객체를 생성할 수 있어야 한다.
 //    따라서 기본 생성자가 필요하다.
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
