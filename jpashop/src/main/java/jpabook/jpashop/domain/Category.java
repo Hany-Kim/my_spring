f28a68420c80101af14b6297a8b05c4354c40690
Hany-Kim <hanykim1210@gmail.com>
2025-07-14 16:26:35 +0900

    실전 예제3 - 다양한 연관관계 매핑 (N:1)

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+}
