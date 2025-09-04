358642a22ae5709fda2ce4e395825f5c25635b8e
Hany-Kim <hanykim1210@gmail.com>
2025-09-05 02:18:13 +0900

    JPQL 프로젝트 세팅

---
+++ jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -0,0 +1,38 @@
+package hellojpa.jpql;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        // 애플리케이션 로딩 시점에 딱 한개만 만들어 둔다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 트랜잭션 단위로 EntityManager를 생성하고 소멸시킨다.
+        EntityManager em = emf.createEntityManager(); // DB 커넥션 하나를 받은것과 유사
+        // EntityManager는 쓰레드간 절대 공유하면 안된다.
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ jpql/src/main/java/hellojpa/jpql/Member.java
@@ -0,0 +1,44 @@
+package hellojpa.jpql;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
