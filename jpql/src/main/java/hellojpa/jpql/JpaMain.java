4d1690864dbfd7a56e8a944af822ac97189e559b
Hany-Kim <hanykim1210@gmail.com>
2025-09-06 01:46:59 +0900

    JPQL 문법

--- jpql/src/main/java/hellojpa/jpql/JpaMain.java
+++ jpql/src/main/java/hellojpa/jpql/JpaMain.java
@@ -23,8 +23,16 @@ public class JpaMain {
         try {
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
 
+            /*
+            * JPQL 문법
+            * - 엔티티와 속성은 대소문자 구분 O (Member, username)
+            * - JPQL키워드는 대소문자 구분 X (SELECT, FROM, WHERE, ...)
+            * - 엔티티 이름 사용, 테이블 이름이 아님(Member)
+            * - 별칭은 필수(m) (as 생략가능)
+            * */
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
